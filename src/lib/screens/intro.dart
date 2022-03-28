93a40ecb729331d3bdc1d8fdd4b45fd2502751e7
André Luiz <an3dree@gmail.com>
2022-03-28 12:17:01 -0300

    theming layout and add bottom navigation

--- src/lib/screens/intro.dart
+++ src/lib/screens/intro.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../shared/drawer.dart';
+import '../shared/bottom.dart';
 
 class IntroScreen extends StatelessWidget {
   const IntroScreen({Key? key}) : super(key: key);
@@ -7,7 +9,32 @@ class IntroScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home?')),
-      body: Center(child: Text('Hello World men')),
+      bottomNavigationBar: BottomMenu(),
+      drawer: MenuDrawer(),
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('assets/universe.jfif'),
+          fit: BoxFit.cover,
+        )),
+        child: Center(
+            child: Container(
+          padding: EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            color: Colors.white60,
+          ),
+          child: Text(
+              'Hello World men, telinha introdutória para alguns exercicios',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 22, color: Colors.black, shadows: [
+                Shadow(
+                    offset: Offset(1.0, 1.0),
+                    blurRadius: 2.0,
+                    color: Colors.greenAccent)
+              ])),
+        )),
+      ),
     );
   }
 }
