fd5077e743f708b81353c3d75783f63bc76c9013
André Luiz <an3dree@gmail.com>
2022-03-28 12:17:19 -0300

    call bottom navigator

--- src/lib/screens/bmi.dart
+++ src/lib/screens/bmi.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../shared/drawer.dart';
+import '../shared/bottom.dart';
 
 class BmiScreen extends StatelessWidget {
   const BmiScreen({Key? key}) : super(key: key);
@@ -7,6 +9,8 @@ class BmiScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('BMI Calculator')),
+      bottomNavigationBar: BottomMenu(),
+      drawer: MenuDrawer(),
       body: Center(child: FlutterLogo()),
     );
   }
