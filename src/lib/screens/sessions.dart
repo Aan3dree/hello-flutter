4a886c42458a8e5e9be00ff3882a24a871cf8168
André Luiz <an3dree@gmail.com>
2022-03-28 22:45:23 -0300

    need fix add

--- src/lib/screens/sessions.dart
+++ src/lib/screens/sessions.dart
@@ -10,17 +10,104 @@ class SessionsScreen extends StatefulWidget {
 }
 
 class _SessionsScreenState extends State<SessionsScreen> {
+  List<Session> sessions = [];
+  final TextEditingController txtDescription = TextEditingController();
+  final TextEditingController txtDuration = TextEditingController();
+  final SPHelper helper = SPHelper();
+
+  @override
+  void initState() {
+    helper.init();
+    helper.init().then((value) {
+      updateScreen();
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Sessions'),
       ),
-      body: Container(),
+      body: ListView(
+        children: getContent(),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          showSessionDialog(context);
+        },
       ),
     );
+  }
+
+  Future<dynamic> showSessionDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext contex) {
+          return AlertDialog(
+            title: Text('Insert Training Session'),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextField(
+                    controller: txtDescription,
+                    decoration: InputDecoration(hintText: 'Description'),
+                  ),
+                  TextField(
+                    controller: txtDuration,
+                    decoration: InputDecoration(hintText: 'Duration'),
+                  )
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(contex);
+                  txtDescription.text = '';
+                  txtDuration.text = '';
+                },
+                child: Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: saveSession,
+                child: Text('Save'),
+              )
+            ],
+          );
+        });
+  }
+
+  Future saveSession() async {
+    DateTime now = DateTime.now();
+    String today = '${now.year}-${now.month}-${now.day}';
+    int id = helper.getCounter() + 1;
+    Session newSession = Session(
+        id, today, txtDescription.text, int.tryParse(txtDuration.text) ?? 0);
+    helper.writeSession(newSession).then((_) {
+      updateScreen();
+      helper.setCounter();
+    });
+    txtDescription.text = '';
+    txtDuration.text = '';
+    Navigator.pop(context);
+  }
+
+  List<Widget> getContent() {
+    List<Widget> tiles = [];
+    sessions.forEach((session) {
+      tiles.add(ListTile(
+        title: Text(session.description),
+        subtitle: Text('${session.date} - duration: ${session.duration} min'),
+      ));
+    });
+    return tiles;
+  }
+
+  void updateScreen() {
+    sessions = helper.getSessions();
+    setState(() {});
   }
 }
