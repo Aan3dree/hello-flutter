68e6e4b960b4c38de14e55b62cb81479312cac23
André Luiz <an3dree@gmail.com>
2022-04-05 11:03:21 -0300

    call the local image

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -11,8 +11,7 @@ void main() {
           ),
           body: Center(
             child: Image(
-              image: NetworkImage(
-                  'https://images.unsplash.com/photo-1612151855475-877969f4a6cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&w=1000&q=80'),
+              image: AssetImage('images/diamond.png'),
             ),
           )),
     ),
