cc622bd02a701d68aeb1083c90134eecd40c9929
André Luiz <an3dree@gmail.com>
2022-04-13 21:27:10 -0300

    circle avatar image

--- my_card/lib/main.dart
+++ my_card/lib/main.dart
@@ -12,37 +12,54 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  color: Colors.red,
-                  width: 100,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      color: Colors.yellow,
-                      height: 100,
-                      width: 100,
-                    ),
-                    Container(
-                      color: Colors.yellow.withOpacity(0.5),
-                      height: 100,
-                      width: 100,
-                    )
-                  ],
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: 100,
-                )
-              ]),
-        ),
+            child: Column(
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.blue,
+              backgroundImage: NetworkImage(
+                  'https://media-exp1.licdn.com/dms/image/C4D03AQH1328ONMc2eQ/profile-displayphoto-shrink_200_200/0/1626192148646?e=1655337600&v=beta&t=RZPt9QqH5kLk6DVxu9PziJwi6W8y13tS1T0lEtNZPDY'),
+            ),
+            Text(
+              'André Souza',
+              style: TextStyle(
+                  fontSize: 36.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        )),
         backgroundColor: Colors.teal,
       ),
     );
   }
 }
+
+Widget myLayout = Row(
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Container(
+        color: Colors.red,
+        width: 100,
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            color: Colors.yellow,
+            height: 100,
+            width: 100,
+          ),
+          Container(
+            color: Colors.yellow.withOpacity(0.5),
+            height: 100,
+            width: 100,
+          )
+        ],
+      ),
+      Container(
+        color: Colors.blue,
+        width: 100,
+      )
+    ]);
