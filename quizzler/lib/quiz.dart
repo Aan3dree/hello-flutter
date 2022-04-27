cc941c7919e8b25a4994481e4ff3f49773ea0d21
André Luiz <an3dree@gmail.com>
2022-04-26 22:32:57 -0300

    removing unnecessary braks

--- quizzler/lib/quiz.dart
+++ quizzler/lib/quiz.dart
@@ -1,12 +1,14 @@
 import 'package:quizzler/question.dart';
 
 class Quiz {
+  int _questionNumber = 0;
+
   final List<Question> _questionsList = [
     Question('Some cats are actually allergic to humans', true),
     Question('You can lead a cow down stairs but not up stairs.', false),
     Question('Approximately one quarter of human bones are in the feet.', true),
     Question('A slug\'s blood is green.', true),
-    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was "Moon".', true),
     Question('It is illegal to pee in the Ocean in Portugal.', true),
     Question(
         'No piece of square dry paper can be folded in half more than 7 times.',
@@ -20,7 +22,7 @@ class Quiz {
     Question(
         'The total surface area of two human lungs is approximately 70 square metres.',
         true),
-    Question('Google was originally called \"Backrub\".', true),
+    Question('Google was originally called "Backrub".', true),
     Question(
         'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
         true),
@@ -29,11 +31,17 @@ class Quiz {
         true),
   ];
 
-  String getQuestionText(int questionNumber) {
-    return _questionsList[questionNumber].questionText;
+  void nextQuestion() {
+    if (_questionNumber < _questionsList.length - 1) {
+      _questionNumber++;
+    }
   }
 
-  bool getQuestionAnswer(int questionNumber) {
-    return _questionsList[questionNumber].questionAnswer;
+  String getQuestionText() {
+    return _questionsList[_questionNumber].questionText;
+  }
+
+  bool getQuestionAnswer() {
+    return _questionsList[_questionNumber].questionAnswer;
   }
 }
