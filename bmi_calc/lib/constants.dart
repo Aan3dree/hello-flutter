5a83e5f740e2a0b39f65a9574e9d8ad0336f9aa4
André Luiz <an3dree@gmail.com>
2022-05-04 01:37:01 -0300

    first comit

---
+++ bmi_calc/lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kTextStyles = TextStyle(color: Color(0xFF8D8E98), fontSize: 18);
+const kNumberStyles = TextStyle(fontSize: 50, fontWeight: FontWeight.w900);
+const kBoxSize = 15.0;
+
+const kBottomContainerHeight = 80;
+const kCustomBackgroundColor = Color(0xFF0F1332);
+const kActiveColor = Color(0xFF1D1E33);
+const kInactiveColor = Color(0xFF111328);
+const kBottomContainerColor = Color(0xFFEB1555);
