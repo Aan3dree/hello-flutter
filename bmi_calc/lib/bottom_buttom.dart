ac4e9a28c01e04f9f5f815079b34ad037285e6ca
André Luiz <an3dree@gmail.com>
2022-05-06 00:31:32 -0300

    new component

---
+++ bmi_calc/lib/bottom_buttom.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class BottomButton extends StatelessWidget {
+  const BottomButton({Key? key, required this.buttonText, required this.onTap})
+      : super(key: key);
+
+  final String buttonText;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        color: kBottomContainerColor,
+        margin: EdgeInsets.only(top: 10),
+        height: kBottomContainerHeight.toDouble(),
+        width: double.infinity,
+        child: Center(
+          child: Text(
+            buttonText,
+            style: kButtonText,
+          ),
+        ),
+      ),
+    );
+  }
+}
