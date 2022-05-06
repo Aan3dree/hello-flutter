8a5bc505ae074158b4993a690f696914e2440f8c
André Luiz <an3dree@gmail.com>
2022-05-06 00:31:43 -0300

    update constants

--- bmi_calc/lib/constants.dart
+++ bmi_calc/lib/constants.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 const kTextStyles = TextStyle(color: Color(0xFF8D8E98), fontSize: 18);
 const kNumberStyles = TextStyle(fontSize: 50, fontWeight: FontWeight.w900);
+const kResultValue = TextStyle(fontSize: 80, fontWeight: FontWeight.bold);
+const kResultTitle = TextStyle(fontSize: 50, fontWeight: FontWeight.w600);
+const kResultSubtitle =
+    TextStyle(fontSize: 28, fontWeight: FontWeight.w600, color: Colors.green);
+const kResultText = TextStyle(fontSize: 20);
+const kButtonText = TextStyle(fontSize: 24, fontWeight: FontWeight.w500);
 const kBoxSize = 15.0;
 
 const kBottomContainerHeight = 80;
