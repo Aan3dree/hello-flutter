d7ea46e76f78111b9af8698e926d2fd0a2585f13
André Luiz <an3dree@gmail.com>
2022-05-26 09:58:08 -0300

    spin while geting data

--- clima_app/lib/screens/loading_screen.dart
+++ clima_app/lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import '../screens/location_screen.dart';
+import '../services/weather.dart';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -7,14 +11,27 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    getLocationtData();
+  }
+
+  void getLocationtData() async {
+    var data = await WeatherModel().getLocationWeather();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: data,
+      );
+    }));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
+        child: SpinKitCircle(
+          color: Colors.white,
+          size: 50.0,
         ),
       ),
     );
