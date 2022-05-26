efb3f2a44c3d1ca95bb0739cd5604d6be6557925
André Luiz <an3dree@gmail.com>
2022-05-26 09:57:45 -0300

    updating UI using data from web

--- clima_app/lib/screens/location_screen.dart
+++ clima_app/lib/screens/location_screen.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 import '../utils/constants.dart';
+import '../services/weather.dart';
+import '../screens/city_screen.dart';
 
 class LocationScreen extends StatefulWidget {
+  final locationWeather;
+  LocationScreen({this.locationWeather});
   @override
   _LocationScreenState createState() => _LocationScreenState();
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+  late int temperature;
+  late String weatherIcon;
+  late String city;
+  late String weatherMessage;
+  WeatherModel weather = WeatherModel();
+  @override
+  void initState() {
+    super.initState();
+    updateUI(widget.locationWeather);
+  }
+
+  void updateUI(dynamic weatherData) {
+    setState(() {
+      if (weatherData == null) {
+        temperature = 0;
+        weatherIcon = 'error';
+        weatherMessage = 'error to geting data';
+        return;
+      } else {
+        double temp = weatherData['main']['temp'] - 273.15;
+        temperature = temp.toInt();
+        int condition = weatherData['weather'][0]['id'];
+        weatherIcon = weather.getWeatherIcon(condition);
+        city = weatherData['name'];
+        weatherMessage = '${weather.getMessage(temperature)} in $city';
+      }
+    });
+
+    print(temperature);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,14 +64,29 @@ class _LocationScreenState extends State<LocationScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      var data = await weather.getLocationWeather();
+                      updateUI(data);
+                    },
                     child: Icon(
                       Icons.near_me,
                       size: 50.0,
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      var typedCity = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => CityScreen(),
+                        ),
+                      );
+                      print(typedCity);
+                      if (typedCity != null) {
+                        var data = await weather.getCityWeather(typedCity);
+                        updateUI(data);
+                      }
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
@@ -49,11 +99,11 @@ class _LocationScreenState extends State<LocationScreen> {
                 child: Row(
                   children: <Widget>[
                     Text(
-                      '32°',
+                      '$temperature°',
                       style: kTempTextStyle,
                     ),
                     Text(
-                      '☀️',
+                      weatherIcon,
                       style: kConditionTextStyle,
                     ),
                   ],
@@ -62,7 +112,7 @@ class _LocationScreenState extends State<LocationScreen> {
               Padding(
                 padding: EdgeInsets.only(right: 15.0),
                 child: Text(
-                  "It's 🍦 time in San Francisco!",
+                  weatherMessage,
                   textAlign: TextAlign.right,
                   style: kMessageTextStyle,
                 ),
@@ -74,3 +124,10 @@ class _LocationScreenState extends State<LocationScreen> {
     );
   }
 }
+/* 
+var data = res.body;
+      double temp = jsonDecode(data)['main']['temp'];
+      String weather = jsonDecode(data)['weather'][0]['main'];
+      String city = jsonDecode(data)['name'];
+
+      */
