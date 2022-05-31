0dff6a57338ab94b0141f98877ebd4220a1a9ef7
André Luiz <an3dree@gmail.com>
2022-05-30 21:57:23 -0300

    load spiner

--- flash_chat/lib/screens/login_screen.dart
+++ flash_chat/lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../constants.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
@@ -14,67 +15,77 @@ class _LoginScreenState extends State<LoginScreen> {
   late String email;
   late String password;
   final _auth = FirebaseAuth.instance;
+  bool _loging = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 150.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: _loging,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 150.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 40.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.black),
-              onChanged: (value) {
-                email = value;
-              },
-              decoration:
-                  kInputDecoration.copyWith(hintText: 'Enter your Email'),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              obscureText: true,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.black),
-              onChanged: (value) {
-                password = value;
-              },
-              decoration:
-                  kInputDecoration.copyWith(hintText: 'Enter your password'),
-            ),
-            SizedBox(
-              height: 15.0,
-            ),
-            MyButton(
-                color: Colors.blueAccent,
-                text: 'Log in',
-                onPress: () async {
-                  try {
-                    final user = await _auth.signInWithEmailAndPassword(
-                        email: email, password: password);
-                    if (user != null) {
-                      Navigator.popAndPushNamed(context, ChatScreen.id);
+              SizedBox(
+                height: 40.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black),
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration:
+                    kInputDecoration.copyWith(hintText: 'Enter your Email'),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black),
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration:
+                    kInputDecoration.copyWith(hintText: 'Enter your password'),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              MyButton(
+                  color: Colors.blueAccent,
+                  text: 'Log in',
+                  onPress: () async {
+                    setState(() {
+                      _loging = true;
+                    });
+                    try {
+                      final user = await _auth.signInWithEmailAndPassword(
+                          email: email, password: password);
+                      if (user != null) {
+                        Navigator.popAndPushNamed(context, ChatScreen.id);
+                      }
+                      setState(() {
+                        _loging = false;
+                      });
+                    } catch (e) {
+                      print(e);
                     }
-                  } catch (e) {
-                    print(e);
-                  }
-                }),
-          ],
+                  }),
+            ],
+          ),
         ),
       ),
     );
--- flash_chat/lib/screens/registration_screen.dart
+++ flash_chat/lib/screens/registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'register_screen';
@@ -14,69 +15,78 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   late String email;
   late String password;
   final _auth = FirebaseAuth.instance;
+  bool _saving = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 150.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: _saving,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 150.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 40.0,
-            ),
-            TextField(
-              style: TextStyle(color: Colors.black87),
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration:
-                  kInputDecoration.copyWith(hintText: 'Enter your Email'),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              style: TextStyle(color: Colors.black87),
-              obscureText: true,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration:
-                  kInputDecoration.copyWith(hintText: 'Enter your password'),
-            ),
-            SizedBox(
-              height: 15.0,
-            ),
-            MyButton(
-                color: Colors.blueAccent,
-                text: 'Register',
-                onPress: () async {
-                  //print(email);
-                  //print(password);
-                  try {
-                    final newUser = await _auth.createUserWithEmailAndPassword(
-                        email: email, password: password);
-                    if (newUser != null) {
-                      Navigator.popAndPushNamed(context, ChatScreen.id);
+              SizedBox(
+                height: 40.0,
+              ),
+              TextField(
+                style: TextStyle(color: Colors.black87),
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration:
+                    kInputDecoration.copyWith(hintText: 'Enter your Email'),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                style: TextStyle(color: Colors.black87),
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration:
+                    kInputDecoration.copyWith(hintText: 'Enter your password'),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              MyButton(
+                  color: Colors.blueAccent,
+                  text: 'Register',
+                  onPress: () async {
+                    setState(() {
+                      _saving = true;
+                    });
+                    try {
+                      final newUser =
+                          await _auth.createUserWithEmailAndPassword(
+                              email: email, password: password);
+                      if (newUser != null) {
+                        Navigator.popAndPushNamed(context, ChatScreen.id);
+                      }
+                      setState(() {
+                        _saving = false;
+                      });
+                    } catch (e) {
+                      print(e);
                     }
-                  } catch (e) {
-                    print(e);
-                  }
-                }),
-          ],
+                  }),
+            ],
+          ),
         ),
       ),
     );
